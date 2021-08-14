adb4d0a790f23b361eafac310fb2967f1224548e
Anurag Kumar <anuragsingh4512@gmail.com>
2021-08-15 01:56:03 +0530

    stock price app completed

---
+++ script.js
@@ -0,0 +1,41 @@
+var initialPrice = document.querySelector("#initial-price");
+var quantityStocks = document.querySelector("#stock-quantity");
+var currentPrice = document.querySelector("#current-price");
+var checkBtn = document.querySelector("#check-btn");
+var outputDiv = document.querySelector(".output");
+
+console.log(outputDiv);
+
+
+
+
+function calculateProfitAndLoss(initial, quantity, current){
+
+    if(initial > current){
+        var loss = (initial - current)* quantity;
+        var lossPercent = (loss/(initial*quantity))* 100;
+        outputDiv.innerText = ` Hey loss is ₹${loss} and loss percent is ${lossPercent}%`;
+    }
+    else if(initial < current){
+        var profit = (current - initial)* quantity;
+        var profitPercent = (profit/(initial* quantity))* 100;
+       outputDiv.innerText = `Hey your profit is ₹${profit} and profit percent is ${profitPercent}%`;
+    }
+    else{
+        outputDiv.innerText= "Lala Risk hai to Ishq hai";
+    }
+}
+
+function clickHandler(){
+    console.log("clicked");
+    var initialPriceValue = Number(initialPrice.value);
+    var quantityStocksValue = Number(quantityStocks.value);
+    var currentPriceValue = Number(currentPrice.value);
+
+    calculateProfitAndLoss(initialPriceValue, quantityStocksValue, currentPriceValue);
+}
+
+
+checkBtn.addEventListener('click', clickHandler);
+
+
